d4d4be35846501afd1bdaa4f8ac93499c8ba7d84
JoaoFontanella <joaofontanella31@gmail.com>
2025-06-20 23:06:09 -0300

    Add Bronze + Alteracoes

---
+++ iac/variables.tf
@@ -0,0 +1,8 @@
+# Lista de variaveis utilizadas nos arquivos de terraform
+variable "resource_group_name" {
+  default = "learn-192a0d72-882a-43ef-9ad7-2b60d58e81f3"
+}
+
+variable "location" {
+  default = "westus"
+}
